ca2241e83f8fc61a85f8be63222ac90b6362a484
kiluadapetrus <matheus.kilua@petrus-software.com>
2025-11-04 22:24:48 -0300

    diretiva

--- app/consultores/page.tsx
+++ app/consultores/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Suspense } from 'react'
 import Link from 'next/link'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
